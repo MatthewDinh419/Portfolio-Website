636a20f3f30f8109bcdace36329072f53921b303
matthewdinh419 <madi0567@colorado.edu>
2020-05-08 19:52:03 -0600

    Set up new website

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { Home } from './Home';
+import { About } from './About';
+import { NoMatch } from './NoMatch';
+import { Layout } from './components/Layout';
+import { NavigationBar } from './components/NavBar';
+
+class App extends Component {
+  render() {
+    return (
+      <React.Fragment>
+        <Router>
+          <NavigationBar />
+          <Layout>
+            <Switch>
+              <Route exact path="/" component={Home} />
+              <Route path="/about" component={About} />
+              <Route component={NoMatch} />
+            </Switch>
+          </Layout>
+        </Router>
+      </React.Fragment>
+    );
+  }
+}
+
+export default App;
